14808d7458aa47e625b212960527e513f5ce0be2
kurbardovikjharis <kurbardovikjharis@gmail.com>
2024-03-18 12:15:25 +0100

    add paging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ dependencies {
 
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.paging.runtime.ktx)
+    implementation(libs.androidx.paging.compose)
 }
 
 // Allow references to generated code
--- app/src/main/java/com/haris/pairing/MainViewModel.kt
+++ app/src/main/java/com/haris/pairing/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.haris.pairing
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flowOf
@@ -10,16 +11,35 @@ import kotlinx.coroutines.flow.stateIn
 import javax.annotation.concurrent.Immutable
 import javax.inject.Inject
 
+@HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
 
     val state: StateFlow<State> = flowOf("haris").map {
-        State(it)
+        State.Success(it)
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
-        initialValue = State()
+        initialValue = State.Empty
     )
 }
 
 @Immutable
-data class State(val state: String = "")
+sealed class State(
+    open val title: String = ""
+) {
+
+    data class Success(
+        override val title: String,
+    ) : State(title)
+
+    data class Error(
+        val message: String,
+        override val title: String,
+    ) : State(title)
+
+    data class Loading(
+        override val title: String,
+    ) : State(title)
+
+    data object Empty : State()
+}
